1b4e2ff6c9b5f152924b358cf8a8e381958b6537
Ilich <ilesrebo.2007@gmail.com>
2025-10-10 15:14:52 -0500

    cambio de base de datos y subida de proyecto a aws

--- platzi/platzi/settings.py
+++ platzi/platzi/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'platzi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ilichuser',
+        'PASSWORD': 'ilichpassword',
+        'HOST': 'bd-platzi-store-ilich.cxmgi8ms0ig3.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
